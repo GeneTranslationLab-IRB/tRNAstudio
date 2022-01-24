f1122b6c4dfafda4ee0d3f9f92fc82b7996dfc81
Marina Murillo Recio <49312084+marinamr@users.noreply.github.com>
2022-01-24 18:10:05 +0100

    Update requirements.sh

--- Scripts/requirements.sh
+++ Scripts/requirements.sh
@@ -16,7 +16,6 @@ then
     #Running the .sh script
     bash Anaconda3-2020.07-Linux-x86_64.sh
     #Compiling from source
-    echo 'PATH=~/anaconda3/bin:$PATH' >> ~/.bashrc
     export PATH=~/anaconda3/bin:$PATH
     source ~/.bashrc
     rm Anaconda3-2020.07-Linux-x86_64.sh
