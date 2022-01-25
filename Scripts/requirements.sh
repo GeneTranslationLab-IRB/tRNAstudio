3b2501795c2571d725fd33ecd0c7d7680370d3fd
Marina Murillo Recio <49312084+marinamr@users.noreply.github.com>
2022-01-25 10:30:39 +0100

    Update requirements.sh

--- Scripts/requirements.sh
+++ Scripts/requirements.sh
@@ -31,8 +31,6 @@ then
     brew install wget
     #Download anaconda file for MAC
     wget https://repo.anaconda.com/archive/Anaconda3-2020.07-MacOSX-x86_64.sh
-    #Checking the integrity of the file
-    sha256sum Anaconda3-2020.07-MacOSX-x86_64.sh
     #Running the .sh script
     bash Anaconda3-2020.07-MacOSX-x86_64.sh
     #Compiling from source
